fd245f7327b3bb8088b6cc16091bc929ab1e9d9a
Tyler Scott <tybugman@hotmail.com>
2016-11-11 16:27:59 -0700

    more

--- load.py
+++ load.py
@@ -9,12 +9,19 @@ def get_column(df, col):
         index = df.columns.values.tolist().index(col)
     return df.values[:,index]
 
-df = pd.read_csv('datasets/SongCSV3.csv')
-#columns = df.columns.values
-print(df.columns.values)
-#data = df.values
-np.set_printoptions(threshold=np.inf)
-column = get_column(df, 'ArtistName')
-for i in column:
-    print(i)
-    
+df = pd.read_csv('datasets/EvolutionPopUSA.csv')
+columns1 = df.columns.values[:10]
+print(columns1)
+columns2 = df.columns.values[-9:]
+print(columns2)
+columns = np.concatenate((columns1,columns2), axis=0)
+# print(df.columns.values)
+data1 = df.values[:,:9]
+data2 = df.values[:,-9]
+data = np.concatenate((data1,data2), axis=0)
+
+print(data)
+column = np.unique(get_column(df, 'artist_name_clean'))
+print(column)
+# for i in column:
+#     print(i)
