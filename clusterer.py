ab08f59dde880a661264727ad7fd8d90ac1f2306
John Meyer <johnny@meyer.net>
2016-12-06 15:28:21 -0700

    added the clustering. It is working with iris data

--- clusterer.py
+++ clusterer.py
@@ -1,5 +1,26 @@
-from sklearn import cluster
+from sklearn.cluster import KMeans
 from sklearn.datasets.base import Bunch
 from sklearn.preprocessing import normalize
 import pandas as pd
 import numpy as np
+
+from sklearn import datasets
+
+
+# get the data set
+data = datasets.load_iris().data
+
+k = KMeans(n_clusters=3).fit(data)
+
+# the index into data fo the song we are looking for
+index = len(data) - 1
+
+cluster_number = k.labels_[index]
+
+# get the index of each item in the cluster
+similar_songs = [i for i, item in enumerate(k.labels_) if item == cluster_number]
+
+print(similar_songs)
+
+# loop through similar_songs and print out the song 
+#   that is associated with that index
